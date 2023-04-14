157d625d540816ce462d2460bd989cd0388b5db6
aksh10-at <aksh10.at@gmail.com>
2023-04-14 20:06:21 +0530

    Navigation View Integration

--- app/src/main/java/com/akshdev/getaway/User/UserDashboard.java
+++ app/src/main/java/com/akshdev/getaway/User/UserDashboard.java
@@ -11,6 +11,6 @@ public class UserDashboard extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_user_dashboard);
+        setContentView(R.layout.dash_user_activity);
     }
 }
